90b1b8477d86779f8f4060a138cfd9c18c32a97a
Killian Demeillers <demeillerskillian@gmail.com>
2024-12-06 15:34:54 +0100

    Modification Ressource

--- qcmBuilder/src/metier/Ressource.java
+++ qcmBuilder/src/metier/Ressource.java
@@ -1,5 +1,6 @@
 package metier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ressource
@@ -7,15 +8,40 @@ public class Ressource
 	private String nom;
 	private List<Notion> notions;
 
-	public Ressource(String nom, List<Notion> notions)
+	public Ressource(String nom)
 	{
 		this.nom = nom;
-		this.notions = notions;
+		this.notions = new ArrayList<>();
 	}
 
 	public String getNom() { return nom; }
 	public List<Notion> getNotions() { return notions; }
 
-	public void setNom(String nom) { this.nom = nom; }
-	public void setNotions(List<Notion> notions) { this.notions = notions; }
+	public boolean setNom(String nom) 
+	{ 
+		this.nom = nom; 
+		return true;
+	}
+
+	public boolean ajouterNotion(Notion notion)
+	{
+		if (notion == null)
+			return false;
+		if (notions.contains(notion))
+			return false;
+		notions.add(notion);
+		return true;
+	}
+
+	public boolean supprimerNotion(Notion notion)
+	{
+		if (notion == null)
+			return false;
+		if (!notions.contains(notion))
+			return false;
+		notions.remove(notion);
+		return true;
+	}
+
+	
 }
