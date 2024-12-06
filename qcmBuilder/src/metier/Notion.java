52ebe99f5c5d023ca3c3d5d9f1179a5ef529961b
Killian Demeillers <demeillerskillian@gmail.com>
2024-12-06 14:37:19 +0100

    modif Notion er Ressource

--- qcmBuilder/src/metier/Notion.java
+++ qcmBuilder/src/metier/Notion.java
@@ -1,7 +1,19 @@
-package Metier;
+package metier;
 
 public class Notion 
 {
 	private String nom;
 	private Notion notion;
+
+	public Notion(String nom, Notion notion) 
+	{
+		this.nom = nom;
+		this.notion = notion;
+	}
+
+	public String getNom   () { return nom;    }
+	public Notion getNotion() { return notion; }
+
+	public void setNom   (String nom)    { this.nom    = nom;   }
+	public void setNotion(Notion notion) { this.notion = notion;}
 }
--- qcmBuilder/src/metier/Ressource.java
+++ qcmBuilder/src/metier/Ressource.java
@@ -1,4 +1,4 @@
-package Metier;
+package metier;
 
 import java.util.List;
 
@@ -6,4 +6,16 @@ public class Ressource
 {
 	private String nom;
 	private List<Notion> notions;
+
+	public Ressource(String nom, List<Notion> notions)
+	{
+		this.nom = nom;
+		this.notions = notions;
+	}
+
+	public String getNom() { return nom; }
+	public List<Notion> getNotions() { return notions; }
+
+	public void setNom(String nom) { this.nom = nom; }
+	public void setNotions(List<Notion> notions) { this.notions = notions; }
 }
