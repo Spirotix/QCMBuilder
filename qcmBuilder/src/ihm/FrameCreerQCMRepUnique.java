59ece0986d901bf06a0b2ebb0ec8d2114204f175
evenFanch <evenfanch1@gmail.com>
2024-12-09 15:00:09 +0100

    ajout qcm rep unique

---
+++ qcmBuilder/src/ihm/FrameCreerQCMRepUnique.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class FrameCreerQCMRepUnique extends JFrame
+{
+	private PanelCreerQuestion 		panelQ;
+	private PanelCreerQCMRepUnique 	panel ;
+
+	public FrameCreerQCMRepUnique (PanelCreerQuestion panelQ)
+	{
+		this.panelQ=panelQ;
+
+		
+		this.setTitle   ("Creation d'un QCM a choix unique");
+		this.setSize    ( 500,500  );
+
+		this.panel=new PanelCreerQCMRepUnique(this.panelQ);
+
+		this.add (this.panel);
+
+		this.setVisible(true);
+	}
+
+
+}
--- qcmBuilder/src/ihm/FrameCreerQuestion.java
+++ qcmBuilder/src/ihm/FrameCreerQuestion.java
@@ -1,9 +1,6 @@
-
-
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-
 
 public class FrameCreerQuestion extends JFrame
 {
