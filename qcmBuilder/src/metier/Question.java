eba974fe07245b7cf4d2fad5edcd02c0bdd4c0b6
ThaoDujardin <thaodujardin@gmail.com>
2024-12-09 16:59:12 +0100

    rajout de 'explication' dans Question.java

--- qcmBuilder/src/metier/Question.java
+++ qcmBuilder/src/metier/Question.java
@@ -7,14 +7,16 @@ public abstract class Question
 	protected int           timer;
 	protected double        nbPoint;
 	protected int           difficulte;
+	protected String        explication;
 
-	public Question(Notion notion, String text, int timer, double nbPoint, int difficulte)
+	public Question(Notion notion, String text, int timer, double nbPoint, int difficulte, String explication)
 	{
 		this.notions     = notion;
 		this.text        = text;
 		this.timer       = timer;
 		this.nbPoint     = nbPoint;
 		this.difficulte  = difficulte;
+		this.explication = explication;
 	}
 	
 	public String        getText()        { return this.text;        }
@@ -22,10 +24,13 @@ public abstract class Question
 	public double        getNbPoint()     { return this.nbPoint;     }
 	public int           getDifficulte()  { return this.difficulte;  }
 	public Notion        getNotions()     { return this.notions;     }
+	public String        getExplication() { return this.explication;        }
+
 
 	public void setText        (String text              ) { this.text        = text;        }
 	public void setTimer       (int timer                ) { this.timer       = timer;       }
 	public void setNbPoint     (int nbPoint              ) { this.nbPoint     = nbPoint;     }
 	public void setDifficulte  (int difficulte           ) { this.difficulte  = difficulte;  }
 	public void setNotions     (Notion notions           ) { this.notions     = notions;     }
+	public void setExplication (String explication       ) { this.explication = explication; }
 }
