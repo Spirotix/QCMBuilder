1bf543117343cbf8b8972788f01fb664078acaf9
Killian Demeillers <demeillerskillian@gmail.com>
2024-12-09 11:04:08 +0100

    Erreur enlevée

--- qcmBuilder/src/Controleur.java
+++ qcmBuilder/src/Controleur.java
@@ -1,4 +1,7 @@
-import metier.QCMBuilder;
+import java.util.ArrayList;
+import metier.*;
+
+
 
 public class Controleur
 {
@@ -8,7 +11,79 @@ public class Controleur
 	public Controleur() 
 	{
 		qcmBuilder = new QCMBuilder();
+	}
 
+	public ArrayList<String> getChoixNotion(String s)
+	{
+		ArrayList<String> notions = new ArrayList<>();
+
+		Ressource r = qcmBuilder.rechercherRessource(s);
+		for (Notion n : r.getNotions())
+		{
+			notions.add(n.getNom());
+		}
 		
+		return notions;
+	}
+
+	public ArrayList<String> getChoixRessources()
+	{
+		ArrayList<String> ressources = new ArrayList<>();
+		for (Ressource r : qcmBuilder.getRessources())
+		{
+			ressources.add(r.getNom());
+		}
+		return ressources;
+	}
+
+	public boolean ajouterRessource(String nom)
+	{
+		return qcmBuilder.ajouterRessource(new Ressource(nom));
+	}
+
+	public boolean supprimerRessource(String nom)
+	{
+		return qcmBuilder.supprimerRessource(qcmBuilder.rechercherRessource(nom));
+	}
+
+	public boolean ajouterNotion(String nomRessource, String nomNotion)
+	{
+		Ressource r = qcmBuilder.rechercherRessource(nomRessource);
+		return r.ajouterNotion(new Notion(nomNotion, r));
+	}
+
+	public boolean supprimerNotion(String nomRessource, String nomNotion)
+	{
+		Ressource r = qcmBuilder.rechercherRessource(nomRessource);
+		return r.supprimerNotion(r.rechercherNotion(nomNotion));
+	}
+
+	public boolean ajouterQuestion(String type, String nomRessource, String nomNotion, String text, int timer, int nbPoint, int nbIndiceUtilisé, int difficulte)
+	{
+		Notion n = qcmBuilder.rechercherRessource(nomRessource).rechercherNotion(nomNotion);
+		Question q = null;
+
+
+		switch (type) {
+			case "QCM" -> {
+				q = new QCM(n, 0, text, timer, nbPoint, nbIndiceUtilisé, difficulte);
+			}
+
+			case "Association" -> {
+				q = new Association(n, 0, text, timer, nbPoint, nbIndiceUtilisé, difficulte);
+			}
+
+			case "Elimination" -> {
+				q = new Elimination(n, 0, text, timer, nbPoint, nbIndiceUtilisé, difficulte);
+			}
+		}
+
+		return n.ajouterQuestion(q);
+	}
+
+	public boolean supprimerQuestion(String nomRessource, String nomNotion, String text)
+	{
+		Notion n = qcmBuilder.rechercherRessource(nomRessource).rechercherNotion(nomNotion);
+		return n.supprimerQuestion(n.rechercherQuestion(text));
 	}
 }
