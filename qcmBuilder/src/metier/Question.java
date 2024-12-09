8aa62e4d128898aeb97a2e759a130e2fb7c43648
ThaoDujardin <thaodujardin@gmail.com>
2024-12-09 14:19:06 +0100

    Question à la Perfection (pour de vrai)

--- qcmBuilder/src/metier/Question.java
+++ qcmBuilder/src/metier/Question.java
@@ -23,19 +23,19 @@ public abstract class Question
 		this.lstReponses     = lstReponse;
 	}
 
-	public String        getText()           { return this.text;            }
-	public int           getTimer()          { return this.timer;           }
-	public double        getNbPoint()        { return this.nbPoint;         }
-	public int           getNbIndiceUtilisé(){ return this.nbIndiceUtilisé; }
-	public List<Reponse> getLstReponses()    { return this.lstReponses;     }
-	public int           getDifficulte()     { return this.difficulte;      }
-	public Notion        getNotions()        { return this.notions;         }
+	public String        getText()            { return this.text;            }
+	public int           getTimer()           { return this.timer;           }
+	public double        getNbPoint()         { return this.nbPoint;         }
+	public int           getNbIndiceUtilisé() { return this.nbIndiceUtilisé; }
+	public List<Reponse> getLstReponses()     { return this.lstReponses;     }
+	public int           getDifficulte()      { return this.difficulte;      }
+	public Notion        getNotions()         { return this.notions;         }
 
-	public void setText           (String text              ) { this.text            = text;            }
-	public void setTimer          (int timer                ) { this.timer           = timer;           }
-	public void setNbPoint        (int nbPoint              ) { this.nbPoint         = nbPoint;         }
-	public void setNbIndiceUtilisé(int nbIndiceUtilisé      ) { this.nbIndiceUtilisé = nbIndiceUtilisé; }
-	public void setDifficulte     (int difficulte           ) { this.difficulte      = difficulte;      }
-	public void setLstReponses    (List<Reponse> lstReponses) { this.lstReponses     = lstReponses;     }
-	public void setNotions        (Notion notions           ) { this.notions         = notions;         }
+	public void setText            (String text              ) { this.text            = text;            }
+	public void setTimer           (int timer                ) { this.timer           = timer;           }
+	public void setNbPoint         (int nbPoint              ) { this.nbPoint         = nbPoint;         }
+	public void setNbIndiceUtilisé (int nbIndiceUtilisé      ) { this.nbIndiceUtilisé = nbIndiceUtilisé; }
+	public void setDifficulte      (int difficulte           ) { this.difficulte      = difficulte;      }
+	public void setLstReponses     (List<Reponse> lstReponses) { this.lstReponses     = lstReponses;     }
+	public void setNotions         (Notion notions           ) { this.notions         = notions;         }
 }
