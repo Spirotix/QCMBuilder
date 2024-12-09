5ff27b36b2b1f53b32d51337c0e7a3e135181e0b
Killian Demeillers <demeillerskillian@gmail.com>
2024-12-09 15:38:15 +0100

    genererQuestionnaire

--- qcmBuilder/src/metier/QCMBuilder.java
+++ qcmBuilder/src/metier/QCMBuilder.java
@@ -2,6 +2,8 @@ package metier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -71,5 +73,68 @@ public class QCMBuilder
 				ressourceTrouvee = ressource;
 		}
 		return ressourceTrouvee;
+	}
+
+	public boolean modifierRessource(Ressource ressource, String nouveauNom)
+	{
+		if (ressource == null)
+			return false;
+		if (!ressources.contains(ressource))
+			return false;
+		ressource.setNom(nouveauNom);
+		return true;
+	}
+
+	public void genererQuestionnaire()
+	{
+		try {
+			File index = new File("../output/index.html");
+			if (!index.exists()) {
+				index.createNewFile();
+			}
+
+			String sIndex = "<!DOCTYPE html>" + //
+							"<html lang=\"fr\">" + //
+							"<head>" + //
+							"<meta charset=\"UTF-8\">" + //
+							"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" + //
+							"<link rel=\"stylesheet\" href=\"index.css\">" + //
+							"<title>Accueil Questionnaire</title>" + //
+							"</head>" + //
+							"<body>" + //
+							"" + //
+							"<h1 class=\"evaluation-title\">Titre du questionnaire</h1>" + //
+							"" + //
+							"<div class=\"informations\">" + //
+							"<p class=\"estimated-time\">Temps estimé : <span class=\"estimated-time-data\">20h</span></p>" + //
+							"" + //
+							"<p class=\"resource\">Ressources concernée : <span class=\"resource-data\">r1.01</span></p>" + //
+							"" + //
+							"<p class=\"notions-title\">Notion abordées :</p>" + //
+							"<ul class=\"notions-list\">" + //
+							"<li class=\"notion-item\"><span class=\"notions-item-data\">Notion 1</span></li>" + //
+							"<li class=\"notion-item\"><span class=\"notions-item-data\">Notion 2</span></li>" + //
+							"<li class=\"notion-item\"><span class=\"notions-item-data\">Notion 3</span></li>" + //
+							"</ul>" + //
+							"" + //
+							"<p class=\"questions-count\">Nombre de questions : <span class=\"questions-count-data\">10</span></p>" + //
+							"<ul class=\"difficulty-list\">" + //
+							"<li class=\"difficulty-item very-easy\">Très facile : <span class=\"difficulty-item-data\">0</span></li>" + //
+							"<li class=\"difficulty-item easy\">     Facile :      <span class=\"difficulty-item-data\">3</span></li>" + //
+							"<li class=\"difficulty-item medium\">   Moyenne :     <span class=\"difficulty-item-data\">4</span></li>" + //
+							"<li class=\"difficulty-item hard\">     Difficile :   <span class=\"difficulty-item-data\">3</span></li>" + //
+							"</ul>" + //
+							"<button type=\"button\" class=\"start-button\">Commencer</button>" + //
+							"</div>" + //
+							"<script src=\"script/index.js\"></script>" + //
+							"</body>" + //
+							"</html>";
+
+			PrintWriter writer = new PrintWriter(index);
+			writer.println(sIndex);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
