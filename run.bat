4dbc3c997f786cd5d679e51bb1a33134d55f651e
Spirotix <theoharel02@gmail.com>
2024-12-10 14:01:48 +0100

    tabulation run

--- run.bat
+++ run.bat
@@ -2,23 +2,23 @@
 
 REM Vérifie si les fichiers arg.list et compile.list existent
 if exist arg.list (
-    if exist compile.list (
-        echo Compilation en cours...
-        javac @arg.list @compile.list
+	if exist compile.list (
+		echo Compilation en cours...
+		javac @arg.list @compile.list
 
-        REM Vérifie si la compilation a réussi
-        if %errorlevel%==0 (
-            echo Compilation réussie. Exécution de src/Controleur...
-            java src/Controleur
-        ) else (
-            echo Erreur : La compilation a échoué.
-            exit /b 1
-        )
-    ) else (
-        echo Erreur : Le fichier compile.list est manquant.
-        exit /b 1
-    )
+		REM Vérifie si la compilation a réussi
+		if %errorlevel%==0 (
+			echo Compilation réussie. Exécution de src/Controleur...
+			java src/Controleur
+		) else (
+			echo Erreur : La compilation a échoué.
+			exit /b 1
+		)
+	) else (
+		echo Erreur : Le fichier compile.list est manquant.
+		exit /b 1
+	)
 ) else (
-    echo Erreur : Le fichier arg.list est manquant.
-    exit /b 1
+	echo Erreur : Le fichier arg.list est manquant.
+	exit /b 1
 )
