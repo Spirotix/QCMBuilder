ee603e511e036ef007b47900ed8d0dc935c0a200
Spirotix <theoharel02@gmail.com>
2024-12-11 09:07:47 +0100

    test affichage des notion d'une ressource

--- qcmBuilder/src/test/AffichageNotion.java
+++ qcmBuilder/src/test/AffichageNotion.java
@@ -1,10 +1,16 @@
 package src.test;
 
 import src.metier.Notion;
+import src.metier.Ressource;
+
 public class AffichageNotion 
 {
 	public static void main(String[] args) 
 	{
-		Notion n1 = new Notion("R1.11 Bases de la communication", "TD1");
+		Ressource r1 = new Ressource("R1.11 Bases de la communication");
+		for ( Notion n : r1.getNotions() )
+		{
+			System.out.println(n.getNom());
+		}
 	}
 }
