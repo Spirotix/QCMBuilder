7de87aa973fb98727f5b907006fc03ef6e3254e0
Spirotix <theoharel02@gmail.com>
2024-12-11 09:00:52 +0100

    Creation du dossier test et début du test AffichageNotion

--- qcmBuilder/src/metier/Elimination.java
+++ qcmBuilder/src/metier/Elimination.java
@@ -40,4 +40,6 @@ public class Elimination extends Question
 		}
 	}
 
+	
+
 }
---
+++ qcmBuilder/src/test/AffichageNotion.java
@@ -0,0 +1,10 @@
+package src.test;
+
+import src.metier.Notion;
+public class AffichageNotion 
+{
+	public static void main(String[] args) 
+	{
+		Notion n1 = new Notion("R1.11 Bases de la communication", "TD1");
+	}
+}
