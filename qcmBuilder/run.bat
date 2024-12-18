2223c1f9f511dd7c75d269044c47fd83da2de2a8
ThaoDujardin <thaodujardin@gmail.com>
2024-12-18 13:42:39 +0100

    .bat*

--- qcmBuilder/run.bat
+++ qcmBuilder/run.bat
@@ -1,25 +1,39 @@
 @echo off
-
 REM Vérifie si les fichiers arg.list et compile.list existent
-if exist arg.list (
-	if exist compile.list (
-		echo Compilation en cours...
-		javac @arg.list @compile.list
+IF EXIST "arg.list" IF EXIST "compile.list" (
+	echo Compilation en cours...
+	javac @arg.list @compile.list
 
-		REM Vérifie si la compilation a réussi
-		if %errorlevel%==0 (
-			echo Compilation réussie. Exécution de src/Controleur...
-			cd class
-			java src/Controleur
-		) else (
-			echo Erreur : La compilation a échoué.
-			exit /b 1
+	REM Vérifie si la compilation a réussi
+	IF %ERRORLEVEL% EQU 0 (
+		echo Compilation reussie.
+
+		REM Générer la Javadoc
+		IF EXIST "javadoc.list" (
+			echo Generation de la Javadoc...
+			IF NOT EXIST "doc" mkdir doc
+			javadoc -d doc @javadoc.list
+
+			IF %ERRORLEVEL% EQU 0 (
+				echo Javadoc generee avec succes dans le dossier 'doc'.
+			) ELSE (
+				echo Erreur : La generation de la Javadoc a echoue.
+				EXIT /B 1
+			)
+		) ELSE (
+			echo Erreur : Le fichier javadoc.list est manquant.
+			EXIT /B 1
 		)
-	) else (
-		echo Erreur : Le fichier compile.list est manquant.
-		exit /b 1
+
+		REM Execution de la classe principale
+		echo Execution de src/Controleur...
+		cd class
+		java src.Controleur
+	) ELSE (
+		echo Erreur : La compilation a echoue.
+		EXIT /B 1
 	)
-) else (
-	echo Erreur : Le fichier arg.list est manquant.
-	exit /b 1
+) ELSE (
+	echo Erreur : Les fichiers arg.list ou compile.list sont manquants.
+	EXIT /B 1
 )
