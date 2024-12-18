ab753db1e849c50500f7985877cb6770a089c1a0
Spirotix <theoharel02@gmail.com>
2024-12-18 09:15:18 +0100

    java doc ReponseAsoos

--- qcmBuilder/src/metier/reponse/ReponseAssociation.java
+++ qcmBuilder/src/metier/reponse/ReponseAssociation.java
@@ -1,21 +1,54 @@
 package src.metier.reponse;
 
+/**
+ * Classe ReponseAssociation représentant une réponse associée dans un QCM.
+ * Hérite de la classe Reponse.
+ */
 public class ReponseAssociation extends Reponse
 {
 	private ReponseAssociation reponseAssocie;
-	private boolean            aGauche;
-	
+	private boolean aGauche;
+
+	/**
+	 * Constructeur de la classe ReponseAssociation.
+	 *
+	 * @param textReponse      Le texte de la réponse.
+	 * @param reponseAssocie   La réponse associée.
+	 * @param aGauche          Indique si la réponse est à gauche.
+	 */
 	public ReponseAssociation(String textReponse, ReponseAssociation reponseAssocie, boolean aGauche)
 	{
 		super(textReponse);
 
 		this.reponseAssocie = reponseAssocie;
-		this.aGauche        = aGauche;
+		this.aGauche = aGauche;
 	}
 
-	public ReponseAssociation getReponseAssocie()                                  { return reponseAssocie;                }
-	public void               setReponseAssocie(ReponseAssociation reponseAssocie) { this.reponseAssocie = reponseAssocie; }
+	/**
+	 * Retourne la réponse associée.
+	 *
+	 * @return La réponse associée.
+	 */
+	public ReponseAssociation getReponseAssocie() { return reponseAssocie; }
 
-	public boolean            estAGauche()                                         { return aGauche;                       }
-	public void               setAGauche(boolean aGauche)                          { this.aGauche = aGauche;               }
+	/**
+	 * Définit la réponse associée.
+	 *
+	 * @param reponseAssocie La nouvelle réponse associée.
+	 */
+	public void setReponseAssocie(ReponseAssociation reponseAssocie) { this.reponseAssocie = reponseAssocie; }
+
+	/**
+	 * Indique si la réponse est à gauche.
+	 *
+	 * @return true si la réponse est à gauche, false sinon.
+	 */
+	public boolean estAGauche() { return aGauche; }
+
+	/**
+	 * Définit si la réponse est à gauche.
+	 *
+	 * @param aGauche true si la réponse est à gauche, false sinon.
+	 */
+	public void setAGauche(boolean aGauche) { this.aGauche = aGauche; }
 }
