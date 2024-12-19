6b1cf4f5ca4da8599100c62c96fb90c474a27fca
ThaoDujardin <thaodujardin@gmail.com>
2024-12-19 15:30:28 +0100

    alignement et suppression des tabs, maintenant bien fais et avec des espaces

--- qcmBuilder/src/ihm/TextFieldPerso.java
+++ qcmBuilder/src/ihm/TextFieldPerso.java
@@ -10,40 +10,40 @@ public class TextFieldPerso extends JTextField
 
 	private String chaine;
 
-	public TextFieldPerso(String chaine) 
+	public TextFieldPerso(String chaine)
 	{
 		this.chaine = chaine;
 
-		this.addFocusListener(new FocusAdapter() 
+		this.addFocusListener(new FocusAdapter()
 		{
 			@Override
-			public void focusGained(FocusEvent e) 
+			public void focusGained(FocusEvent e)
 			{
-				if (getText().isEmpty()) 
+				if (getText().isEmpty())
 					repaint();
 			}
 
 			@Override
-			public void focusLost(FocusEvent e) 
+			public void focusLost(FocusEvent e)
 			{
-				if (getText().isEmpty()) 
+				if (getText().isEmpty())
 					repaint();
 			}
 		});
 	}
 
 	@Override
-	protected void paintComponent(Graphics g) 
+	protected void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 
-		if (getText().isEmpty() && !isFocusOwner()) 
+		if (getText().isEmpty() && !isFocusOwner())
 		{
 			Graphics2D g2 = (Graphics2D) g;
 
 			g2.setRenderingHint (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			g2.setColor			(Color.GRAY); 
-			g2.setFont 			(getFont().deriveFont(Font.ITALIC)); 
+			g2.setColor         (Color.GRAY);
+			g2.setFont          (getFont().deriveFont(Font.ITALIC));
 
 			Insets insets = getInsets();
 			g2.drawString(this.chaine, insets.left, getHeight() / 2 + g2.getFontMetrics().getAscent() / 2 - 2);
