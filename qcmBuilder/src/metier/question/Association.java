18b3db92a223274fd3ef8b511285bda3155faedb
Spirotix <theoharel02@gmail.com>
2024-12-20 13:33:23 +0100

    ça marche

--- qcmBuilder/src/metier/question/Association.java
+++ qcmBuilder/src/metier/question/Association.java
@@ -41,7 +41,6 @@ public class Association implements Question
 		this.nbPoint        = nbPoint;
 		this.difficulte     = difficulte;
 		this.lstReponseAsso = new ArrayList<>(lstReponseAsso);
-		Collections.shuffle(this.lstReponseAsso);
 		this.explication    = explication;
 		this.urlImage       = "";
 	}
