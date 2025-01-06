26c1ac65f76cfb3c9f4699b2078efbaf01bb8a80
Fanch Even <fancheventravail@gmail.com>
2025-01-06 09:10:00 +0100

    textfied perso correction

--- qcmBuilder/src/ihm/TextFieldPerso.java
+++ qcmBuilder/src/ihm/TextFieldPerso.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.geom.RoundRectangle2D;
 
 public class TextFieldPerso extends JTextField 
 {
@@ -14,8 +13,6 @@ public class TextFieldPerso extends JTextField
 	public TextFieldPerso(String chaine)
 	{
 		this.chaine = chaine;
-		setOpaque(false);
-		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 		this.addFocusListener(new FocusAdapter()
 		{
@@ -40,28 +37,16 @@ public class TextFieldPerso extends JTextField
 	{
 		super.paintComponent(g);
 
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-		// Background
-		g2.setColor(getBackground());
-		g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 15, 15));
-
-		// Placeholder text
 		if (getText().isEmpty() && !isFocusOwner())
 		{
-			g2.setColor(Color.GRAY);
-			g2.setFont(getFont().deriveFont(Font.ITALIC));
-			g2.drawString(chaine, getInsets().left, g.getFontMetrics().getMaxAscent() + getInsets().top);
-		}
-	}
+			Graphics2D g2 = (Graphics2D) g;
 
-	@Override
-	protected void paintBorder(Graphics g)
-	{
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2.setColor(Color.LIGHT_GRAY);
-		g2.draw(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 15, 15));
+			g2.setRenderingHint (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2.setColor         (Color.GRAY);
+			g2.setFont          (getFont().deriveFont(Font.ITALIC));
+
+			Insets insets = getInsets();
+			g2.drawString(this.chaine, insets.left, getHeight() / 2 + g2.getFontMetrics().getAscent() / 2 - 2);
+		}
 	}
 }
