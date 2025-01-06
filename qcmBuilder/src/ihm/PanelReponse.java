448cd816a483c561c1ee830a9c6813058205fa41
Killian Demeillers <demeillerskillian@gmail.com>
2025-01-06 09:13:32 +0100

    prise de l'ancienne version

--- qcmBuilder/src/ihm/PanelReponse.java
+++ qcmBuilder/src/ihm/PanelReponse.java
@@ -1,19 +1,26 @@
 package src.ihm;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import src.ihm.question.*;
+import java.awt.*					;
+import java.awt.event.*				;
+import java.io.File				;
+import java.io.IOException		;
+import javax.imageio.ImageIO					;
+import javax.swing.*			;
+import src.ihm.question.*			;
 
 public class PanelReponse extends JPanel implements ActionListener
 {
 	private PanelCreerQCMRepUnique	panelQ				;
-	private JButton					corbeille			;
+	private JButton					corbeille, importer	;
+	private JLabel 					imageImporter		;
 	private JTextField				contenu				;
 	private JCheckBox 				validationM 		;
 	private JRadioButton 			validationU 		;
 	private String 					type 				;
+	private FileHandler 			fileHandler			;
+	private File 					fileChoisi 			;
 	private int 					indice				;
+	private JPanel 					panelImage 			;
 
 	public PanelReponse (PanelCreerQCMRepUnique panelQ, String type, int indice)
 	{
@@ -21,41 +28,58 @@ public class PanelReponse extends JPanel implements ActionListener
 		this.type 	= type 	 ;
 		this.indice = indice ;
 
+		this.imageImporter 	= new JLabel	  (								);
+		this.fileHandler	= new FileHandler ("fichier_reponse"+this.indice);
+
 		this.setLayout(new GridLayout(1,3));
 
 		//Initialisation
-
-		this.corbeille 	= new JButton("Supprimer");
-		this.corbeille.setPreferredSize(new Dimension(120, 50)); // Increase the width to fit the text
-		this.corbeille.setBackground(new Color(255, 102, 102)); // Set a less bright red color
-		this.corbeille.setForeground(Color.WHITE); // Set the text color to white
+		this.corbeille 	= new JButton(new ImageIcon("../img/poubelle.PNG"	));
+		this.importer	= new JButton(new ImageIcon("../img/inserer.PNG"	));
 
 		this.contenu 	= new TextFieldPerso ("contenu");
-		this.contenu.setPreferredSize(new Dimension(100, 50));
-
-		this.validation = new JCheckBox();
-		this.validation.setPreferredSize(new Dimension(30, 50));
+		
+		
+		if ( this.type.equals("Unique") )
+			this.validationU = new JRadioButton();
+		else
+			this.validationM = new JCheckBox();
 
 		//Insertion
-		JPanel panelGauche = new JPanel(new GridBagLayout());
+		JPanel panelGauche = new JPanel ();
 		panelGauche.add(this.corbeille);
 
-		JPanel panelMilieu = new JPanel(new GridBagLayout());
-		panelMilieu.add(this.contenu);
+		this.add (panelGauche);
+		this.add (this.contenu	 );
 
-		JPanel panelDroite = new JPanel(new GridBagLayout());
-		panelDroite.add(this.validation);
+		JPanel panelDroite	 	= new JPanel (new BorderLayout());
+		JPanel panelDroiteHaut 	= new JPanel ();
+		this.panelImage	= new JPanel ();
+		this.panelImage.setPreferredSize(new Dimension(75, 75));
+		
+		if ( this.type.equals("Unique") )
+			panelDroiteHaut.add ( this.validationU );
+		else
+			panelDroiteHaut.add ( this.validationM );
+		panelDroiteHaut.add (this.importer                       );
+		panelDroite	   .add (panelDroiteHaut, BorderLayout.NORTH );
+		this.panelImage.add (this.imageImporter                  );
+		panelDroite	   .add (this.panelImage, BorderLayout.CENTER);
 
-		this.add(panelGauche);
-		this.add(panelMilieu);
 		this.add(panelDroite);
 
 		//ActionListener / itemListener
 		this.corbeille  .addActionListener(this);
 		this.contenu	.addActionListener(this);
-		this.validation .addActionListener(this);;
+
+		if ( this.type.equals("Unique") )
+			this.validationU.addActionListener(this);
+		else
+			this.validationM.addActionListener(this);
+		this.importer	.addActionListener(this);
 
 		this.setVisible(true);
+
 	}
 
 	public void actionPerformed(ActionEvent e)
@@ -65,7 +89,9 @@ public class PanelReponse extends JPanel implements ActionListener
 			this.panelQ.supprimer(this);
 		}
 
-		if (e.getSource().equals(this.validation))
+		this.updateImage();
+
+		if (e.getSource().equals(this.validationU))
 		{
 			if (this.validationU.isSelected())
 			{
@@ -73,6 +99,40 @@ public class PanelReponse extends JPanel implements ActionListener
 				this.validationU.setSelected (true);
 			}
 		}
+
+		if (e.getSource().equals(this.importer)) 
+		{
+			try 
+			{
+				this.fileChoisi = fileHandler.chooseFile();
+				fileHandler.handleFile(this.fileChoisi);
+				this.updateImage();
+			} 
+			catch (IOException ex)
+			{
+				System.out.println("Erreur lors du traitement du fichier : " + ex.getMessage());
+			}
+			this.repaint();
+		}
+	}
+
+	public void updateImage()
+	{
+		if (this.fileChoisi==null)
+			return ;
+		try 
+		{
+			Image image 		 = ImageIO.read(new File("../data/ressources_notions_questions/temp/fichier_reponse"+this.indice+"."+this.fileHandler.getExtension(this.fileChoisi.getName())));
+			Image imageRetaillee = image.getScaledInstance( this.panelImage.getHeight(), this.panelImage.getHeight(), Image.SCALE_AREA_AVERAGING);
+
+			this.imageImporter.setIcon(new ImageIcon(imageRetaillee));
+			System.out.println(this.imageImporter.getIcon());
+		} 
+		catch (IOException ex) 
+		{
+			System.out.println("Erreur lors du traitement du fichier : " + ex.getMessage());
+		}
+		this.repaint();
 	}
 
 	public void decocher()
@@ -91,7 +151,6 @@ public class PanelReponse extends JPanel implements ActionListener
 		else
 			return this.validationM.isSelected();
 	}
-
 
 	public String getString(){return this.contenu.getText();}
 }
